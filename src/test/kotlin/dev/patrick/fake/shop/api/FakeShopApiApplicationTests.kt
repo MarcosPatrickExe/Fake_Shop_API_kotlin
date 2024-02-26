983e54ef7568ba28353505802830cc30dfeacf87
Analistas de Sistema <analise@redeindustrial.com.br>
2024-02-26 20:54:45 -0300

    initial commit

---
+++ src/test/kotlin/dev/patrick/fake/shop/api/FakeShopApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.patrick.fake.shop.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FakeShopApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
