998c353c452c419153b390a923c37a4698267f04
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-02-26 22:58:26 -0300

    adding annotations to Customer entity

--- src/main/kotlin/dev/patrick/fake/shop/api/entity/Customer.kt
+++ src/main/kotlin/dev/patrick/fake/shop/api/entity/Customer.kt
@@ -1,16 +1,40 @@
 package dev.patrick.fake.shop.api.entity
-
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 
+@Entity( )
+@Table( name="Client ")
 data class Customer(
+
+        @Column( nullable = false )
         var firstName: String = "",
+
+        @Column( nullable = false )
         var lastName: String = "",
+
+        @Column( nullable = false, unique =true )
         val cpf: String = "",
+
+        @Column( nullable = false, unique=true )
         var email: String = "",
+
+        @Column( nullable = false )
         var password :String = "",
+
+        @Column( nullable = false )
+        @Embedded
         val address :Address = Address(),
-        var credits :List<Credit> = mutableListOf(),
+
+
+        @Column( nullable = false )
+        @OneToMany(
+                  fetch = FetchType.LAZY,
+                  cascade = arrayOf(   CascadeType.REMOVE, CascadeType.PERSIST  ),
+                  mappedBy = "customer"
+         )
+        var credits :List<Credit> = mutableListOf<Credit>(),
+
+        @Id  @GeneratedValue( strategy = GenerationType.IDENTITY)
         val id: Long? = null
+     //  will be generated in the future
 )
