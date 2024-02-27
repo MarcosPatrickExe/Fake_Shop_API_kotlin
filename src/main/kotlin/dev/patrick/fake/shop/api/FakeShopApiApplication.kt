bd74891e5f446394743fe5c4f81f540288ef8033
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-02-26 23:15:25 -0300

    adding ddl-auto = CREATE DROP

--- src/main/kotlin/dev/patrick/fake/shop/api/FakeShopApiApplication.kt
+++ src/main/kotlin/dev/patrick/fake/shop/api/FakeShopApiApplication.kt
@@ -1,11 +1,11 @@
 package dev.patrick.fake.shop.api
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
 
 @SpringBootApplication
 class FakeShopApiApplication
 
-fun main(args: Array<String>) {
-  	runApplication<FakeShopApiApplication>(*args)
+fun main( args: Array<String> ) {
+  		runApplication<FakeShopApiApplication>(*args)
 }
