f878099daa99a7992daeba0853ba90679757043d
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-02-26 21:47:08 -0300

    adding the Credit request Status

--- src/main/kotlin/dev/patrick/fake/shop/api/entity/Credit.kt
+++ src/main/kotlin/dev/patrick/fake/shop/api/entity/Credit.kt
@@ -1,5 +1,6 @@
 package dev.patrick.fake.shop.api.entity
 
+import dev.patrick.fake.shop.api.utils.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
@@ -8,7 +9,9 @@ import java.util.UUID
 class Credit (
         val creditCode : UUID = UUID.randomUUID(),
         val creditValue : BigDecimal = BigDecimal.ZERO,
-        val dayFirstInstallment :LocalDate,          // date of first parcel
-        val numberOfInstallments :Int = 0
-
+        val dayFirstInstallment :LocalDate,                   // date of first parcel
+        val numberOfInstallments :Int = 0,
+        val status : Status = Status.PROGRESS,
+        val customer :Customer? = null,                      // willl be created when the Credit instance
+        val  id: Long? = null
 )
---
+++ src/main/kotlin/dev/patrick/fake/shop/api/utils/Status.kt
@@ -0,0 +1,8 @@
+package dev.patrick.fake.shop.api.utils
+
+
+enum class Status {
+       PROGRESS,
+       APPROVED,
+       REJECT
+}
